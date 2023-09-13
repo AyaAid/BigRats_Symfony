c2545e580930b843d99110b6f99afaf71e1ca51b
celianloisel <celianloisel@gmail.com>
2023-09-13 19:44:59 +0200

    Add 404 page if tricount don't existe

--- src/Controller/TricountController.php
+++ src/Controller/TricountController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Tricounts;
 use App\Service\GetTableByIdService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TricountController extends AbstractController
@@ -20,6 +21,12 @@ class TricountController extends AbstractController
     #[Route(path: '/tricount/{tricountId}', name: 'app_tricount')]
     public function index(string $tricountId)
     {
-        return $this->render('tricount.html.twig', ['tricountArray' => $this->GetTableByIdService->getTable(Tricounts::class, $tricountId)]);
+        $tricount = $this->GetTableByIdService->getTable(Tricounts::class, $tricountId);
+
+        if (!$tricount) {
+            return $this->render('page_not_found.twig', ['message' => 'Le tricount n\'existe pas']);
+        }
+
+        return $this->render('tricount.html.twig', ['tricountArray' => $tricount]);
     }
 }
