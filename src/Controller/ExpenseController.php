2e06918fa289841578193ce5c46faedda77cb002
celianloisel <celianloisel@gmail.com>
2023-09-14 01:27:22 +0200

    HOT FIX → body of page was removed. Feat : Refacto route to create expense

--- src/Controller/ExpenseController.php
+++ src/Controller/ExpenseController.php
@@ -2,13 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Expenses;
 use App\Form\ExpensesFormType;
 use App\Service\CreateExpenseService;
-use App\Service\DeleteExpenseService;
-use App\Service\GetExpensesConcernedUserService;
-use App\Service\GetTableByIdService;
-use App\Service\GetTableService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,44 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExpenseController extends AbstractController
 {
     private $createExpenseService;
-    private $GetExpensesConcernedUserService;
-    private $GetTableService;
-    private $GetTableByIdService;
 
-    public function __construct(CreateExpenseService $createExpenseService, GetExpensesConcernedUserService $GetExpensesConcernedUserService, GetTableService $GetTableService, DeleteExpenseService $DeleteExpensesService, GetTableByIdService $GetTableByIdService)
+    public function __construct(CreateExpenseService $createExpenseService)
     {
         $this->createExpenseService = $createExpenseService;
-        $this->GetExpensesConcernedUserService = $GetExpensesConcernedUserService;
-        $this->GetTableService = $GetTableService;
-        $this->DeleteExpenseService = $DeleteExpensesService;
-        $this->GetTableByIdService = $GetTableByIdService;
     }
 
-    #[Route(path: '/tricount/{tricountId}/new-expenses', name: 'app_tricount_expenses')]
+    #[Route(path: '/tricount/{tricountId}/expenses', name: 'app_tricount_expenses')]
     public function spendForm(Request $request, string $tricountId)
     {
-        $expense = new Expenses();
-
-        $form = $this->createForm(ExpensesFormType::class, $expense);
+        $form = $this->createForm(ExpensesFormType::class);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            $this->createExpenseService->createExpense($data, $tricountId);
+            $this->createExpenseService->createExpense($data, $request, $tricountId);
             return $this->redirectToRoute('home_page');
         }
 
         return $this->render('expenses.html.twig', [
             'form' => $form->createView(),
         ]);
-    }
-
-    #[Route(path: '/expenses/{expensesId}', name: 'app_expenses_delete')]
-    public function deleteExpenses(string $expensesId)
-    {
-        $this->DeleteExpenseService->deleteExpense($this->GetTableByIdService->getTable(Expenses::class, $expensesId)[0]);
-
-        return $this->redirectToRoute('home_page');
     }
 }
--- src/Service/CreateExpenseService.php
+++ src/Service/CreateExpenseService.php
@@ -6,12 +6,15 @@ use App\Entity\Expenses;
 use App\Entity\Tricounts;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 class CreateExpenseService
 {
 
     private $security;
+
     private $entityManager;
+
     private $GetTableByIdService;
 
     public function __construct(EntityManagerInterface $entityManager, Security $security, GetTableByIdService $GetTableByIdService)
@@ -21,23 +24,17 @@ class CreateExpenseService
         $this->GetTableByIdService = $GetTableByIdService;
     }
 
-    public function createExpense($data, $tricountId)
+    public function createExpense($data, Request $request, $tricountId)
     {
         $title = $data->getTitle();
         $value = $data->getValue();
         $user = $this->security->getUser();
         $tricount = $this->GetTableByIdService->getTable(Tricounts::class, $tricountId);
-        $concernedUsers = $data->getConcernedUsers();
 
         $expense = new Expenses();
         $expense->setTitle($title);
         $expense->setValue($value);
         $expense->setTricount($tricount[0]);
-
-        foreach ($concernedUsers as $concernedUser) {
-            $expense->addConcernedUser($concernedUser);
-        }
-
         $expense->setCreatedAt(new \DateTimeImmutable());
         $expense->setUser($user);
 
