a61decf9e2fd226a7b58532078c3912716d2e834
AyaAid <aya.lakhel11@gmail.com>
2023-09-13 09:23:12 +0200

    make user replace

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -3,79 +3,33 @@
 namespace App\Entity;
 
 use App\Repository\UsersRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
-class Users
+class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $firstname = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $lastname = null;
-
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $role = null;
-
     #[ORM\Column]
-    private ?\DateTimeImmutable $created_at = null;
+    private array $roles = [];
 
-    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Notifications::class, orphanRemoval: true)]
-    private Collection $notifications;
-
-    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Expenses::class, orphanRemoval: true)]
-    private Collection $expenses;
-
-    #[ORM\ManyToMany(targetEntity: Tricounts::class, mappedBy: 'user')]
-    private Collection $tricounts;
-
-    public function __construct()
-    {
-        $this->notifications = new ArrayCollection();
-        $this->expenses = new ArrayCollection();
-        $this->tricounts = new ArrayCollection();
-    }
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(string $firstname): static
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(string $lastname): static
-    {
-        $this->lastname = $lastname;
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -90,7 +44,39 @@ class Users
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -102,114 +88,12 @@ class Users
         return $this;
     }
 
-    public function getRole(): ?string
-    {
-        return $this->role;
-    }
-
-    public function setRole(string $role): static
-    {
-        $this->role = $role;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->created_at;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $created_at): static
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
     /**
-     * @return Collection<int, Notifications>
+     * @see UserInterface
      */
-    public function getNotifications(): Collection
+    public function eraseCredentials(): void
     {
-        return $this->notifications;
-    }
-
-    public function addNotification(Notifications $notification): static
-    {
-        if (!$this->notifications->contains($notification)) {
-            $this->notifications->add($notification);
-            $notification->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeNotification(Notifications $notification): static
-    {
-        if ($this->notifications->removeElement($notification)) {
-            // set the owning side to null (unless already changed)
-            if ($notification->getUser() === $this) {
-                $notification->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Expenses>
-     */
-    public function getExpenses(): Collection
-    {
-        return $this->expenses;
-    }
-
-    public function addExpense(Expenses $expense): static
-    {
-        if (!$this->expenses->contains($expense)) {
-            $this->expenses->add($expense);
-            $expense->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeExpense(Expenses $expense): static
-    {
-        if ($this->expenses->removeElement($expense)) {
-            // set the owning side to null (unless already changed)
-            if ($expense->getUser() === $this) {
-                $expense->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Tricounts>
-     */
-    public function getTricounts(): Collection
-    {
-        return $this->tricounts;
-    }
-
-    public function addTricount(Tricounts $tricount): static
-    {
-        if (!$this->tricounts->contains($tricount)) {
-            $this->tricounts->add($tricount);
-            $tricount->addUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeTricount(Tricounts $tricount): static
-    {
-        if ($this->tricounts->removeElement($tricount)) {
-            $tricount->removeUser($this);
-        }
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
