72f3dfce38f23e2e4219858d3fce4f6003dfed5f
kilianrdgs <kilian.rdgs@free.fr>
2023-09-14 09:18:43 +0200

    nouvelles pages static et img

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -26,12 +26,24 @@ class TestController extends AbstractController
     #[Route("/authentication", name:"controller_test")]
     public function index(): Response
     {
-        return $this->render('authentication.html.twig', ['name' => 'test']);
+        return $this->render('/static/authentication.html.twig', ['name' => 'test']);
     }
 
     #[Route("/list-tricounts", name:"listtricounts")]
     public function listtricounts(): Response
     {
         return $this->render('list-tricounts.html.twig', ['tricountslist' => $this->GetTableByUserService->getTable(Users::class, $this->GetSessionUserService->getUser())]);
+    }
+
+    #[Route("/error_404", name:"error_404")]
+    public function error(): Response
+    {
+        return $this->render('/static/error_404.html.twig', ['name' => 'test']);
+    }
+
+    #[Route("/login_static", name:"login_static")]
+    public function static(): Response
+    {
+        return $this->render('/static/login_static.html.twig', ['name' => 'test']);
     }
 }
